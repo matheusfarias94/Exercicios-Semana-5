b0663b738e193429bb3ecdfd5a3dc00a9cbfdc46
Matheus Farias <matheusfarias95@hotmail.com>
2024-03-11 22:16:39 -0300

    modified:   "[M1S05] Ex 6 - Coloque na tela o n\303\272mero de produtos presentes no carrinho de forma din\303\242mica/script.js"

--- [M1S05] Ex 6 - Coloque na tela o número de produtos presentes no carrinho de forma dinâmica/script.js
+++ [M1S05] Ex 6 - Coloque na tela o número de produtos presentes no carrinho de forma dinâmica/script.js
@@ -2,6 +2,7 @@
 
 let Carrinho = [];
 
+
 // Captura a identificação do produto a partir de um elemento com a classe 'idProduto'
 function Valor() {
     let identificação = document.querySelector('.idProduto').value
@@ -31,15 +32,24 @@ function adicionar() {
         // Cria um objeto representando o produto a ser adicionado ao carrinho
         let produto1 = {
             nome: nomeProduto,
-            preço: 3.99 // Define o preço do produto (pode ser ajustado conforme necessário)
+            preço: 3.99 // Define o preço do produto 
         };
         let container = document.querySelector('#carrinho');
         let elementoCriado = document.createElement('li');
-
         elementoCriado.textContent = `${produto1.nome} - R$ ${produto1.preço.toFixed(2)}`;
         container.appendChild(elementoCriado);
         // Adiciona o produto ao carrinho
         Carrinho.push(produto1);
+        
+
+        // Armazena o array atualizado no Local Storage
+        localStorage.setItem("meuArray", JSON.stringify(Carrinho));
+        
+        // Recupera o array do Local Storage, se existir
+        let arrayArmazenado = JSON.parse(localStorage.getItem("meuArray"));
+
+
+
 
     }
 
